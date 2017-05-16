e5a8b40df0976f13a9534e7013a52d876f8efa22
Basim Khajwal <basimkhajwal@gmail.com>
2017-05-16 21:06:50 +0100

    Got some better results

--- src/main/scala/Titanic.scala
+++ src/main/scala/Titanic.scala
@@ -1,4 +1,3 @@
-import java.io.PrintWriter
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
 
@@ -15,10 +14,11 @@ object Titanic {
 
   val testData: String = "data/titanic/test.csv"
   val trainData: String = "data/titanic/train.csv"
-  val outputFile: String = "data/titanic/predictionRegChanged.csv"
+  val outputFile: String = "data/titanic/predictionScaled.csv"
 
   case class PassengerData(id: Int, pClass: Int, isMale: Boolean, age: Double, sibSp: Int, parch: Int, fare: Double) extends DataObject {
-    override def getData: Array[Double] = Array(pClass, if (isMale) 0 else 1, sibSp, parch, fare, pClass * fare, 0.01*fare*fare)
+    override def getData: Array[Double] =
+      Array(if (isMale) 200 else 0, fare, pClass * 100, sibSp * 60, parch * 60)
   }
 
   def readColumns(isTest: Boolean, cols: Array[String]): PassengerData = {
@@ -60,23 +60,27 @@ object Titanic {
 
     println(DataObject.getMatrix(trainData))
 
+    val numIterations = 25000
+
     val logModel = new LogisticRegression(DataObject.getMatrix(trainData), DenseVector(predictions.map(x => if (x) 1 else 0)))
     val before: Long = System.currentTimeMillis();
-    val res = logModel.solve(2000, 0.00001, lambda = 0)
+    val res = logModel.solve(numIterations, 0.0001)
     println("Time taken: " + (System.currentTimeMillis() - before) + "ms")
 
     val f = Figure("Cost over iterations")
     val p = f.subplot(0)
-    val x: DenseVector[Double] = DenseVector((1 to 2000).toArray).map(x => 1.0 * x)
-    p += plot(x, res._2, '.')
+    val x: DenseVector[Double] = DenseVector((1 to numIterations).toArray).map(x => 1.0 * x)
+    val y = res._2.map(_ * 100)
+    p += plot(x, y, '.')
     p.xlabel = "Number of Iterations"
     p.ylabel = "Cost"
 
-    for (i <- 1 to 20) println("Iter " + (i * 100) + ": " + res._2.valueAt(i*10))
+    for (i <- 1 to numIterations by 100) println("Iter " + i + ": " + res._2.valueAt(i))
 
     val cp = logModel.hypothesis(res._1)
     val totalCorrect = cp.valuesIterator.zip(predictions.iterator).filter(x => (x._1 >= 0.5) == x._2).length
     println("Training set correctly predicted: " + totalCorrect + " / " + cp.length)
+    println(res._1)
 
     val testData = loadData(true)
     val predVals = logModel.hypothesis(res._1, DataObject.getMatrix(testData)).valuesIterator.map(x => if (x >= 0.5) 1 else 0)
