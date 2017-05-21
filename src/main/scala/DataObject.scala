7a18617ef96baab10cbc3a8adcfc16bf29d1cbfe
Basim Khajwal <basimkhajwal@gmail.com>
2017-05-21 23:30:10 +0100

    Made things a bit more efficient with a better name

--- src/main/scala/DataObject.scala
+++ src/main/scala/DataObject.scala
@@ -1,20 +1,26 @@
 
-import breeze.linalg.{DenseMatrix}
+import breeze.linalg.DenseMatrix
 
 /**
   * Created by Basim on 05/11/2016.
   */
 trait DataObject {
-
-  def getData: Array[Double]
+  def getFeatureVector(): Array[Double]
 }
 
 object DataObject {
-  def getMatrix[U <: DataObject](data: Array[U]): DenseMatrix[Double] = {
-     new DenseMatrix[Double](
-       data(0).getData.length + 1,
-       data.length,
-       data.foldLeft(Array.empty[Double])((arr, obj) => (arr :+ 1.0) ++ obj.getData)
-     ).t // get the transpose
+  def getDataMatrix[U <: DataObject](dataObjects: Array[U]): DenseMatrix[Double] = {
+    val data = dataObjects map (_.getFeatureVector())
+    val dataSize = data(0).length + 1
+    val arrayData = new Array[Double](dataSize * data.length)
+
+    var i = 0
+    while (i < data.length) {
+      arrayData(i * dataSize) = 1
+      System.arraycopy(data(i), 0, arrayData, 1+i*dataSize, dataSize-1)
+      i += 1
+    }
+
+    new DenseMatrix[Double](dataSize, data.length, arrayData).t
   }
 }
